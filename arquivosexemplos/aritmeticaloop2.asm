e691c3b006221338caae67c16ad4f7fc7edf41bb
kourggos <guilhermedhfontoura@gmail.com>
2025-07-22 23:38:18 -0300

    commit final 3

---
+++ arquivosexemplos/aritmeticaloop2.asm
@@ -0,0 +1,40 @@
+addi s0, zero, 3
+addi s1, zero, 5
+addi s2, zero, 2
+addi s3, zero, 1
+nop
+
+add t0, s0, s1
+
+nop
+nop
+
+add t1, s2, s3
+nop
+nop
+nop
+
+sub s4, t0, t1
+addi t0, zero, 1
+addi s3, zero, 0
+addi t1, zero, 4
+nop
+nop
+nop
+
+loop:
+ bge t0, t1, END
+ nop
+ nop
+ nop
+ sw t0, 0(t0)
+
+ add s3, s3, t0
+ nop
+ addi t0, t0, 1
+ nop
+ nop
+ j loop
+ nop
+END:
+
